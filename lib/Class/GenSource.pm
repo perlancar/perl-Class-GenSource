d55b0ce13ef9eeb9af99d60837a2f89cd66f6e98
perlancar (@pc-office) <perlancar@gmail.com>
2015-06-10 15:36:04 +0700

    Initial commit

---
+++ lib/Class/GenSource.pm
@@ -0,0 +1,106 @@
+package Class::GenSource;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+require Exporter;
+our @ISA = qw(Exporter);
+our @EXPORT_OK = qw(gen_class_source_code);
+
+our %SPEC;
+
+my $re_ident = qr/\A[A-Za-z_][A-Za-z0-9_]*(::[A-Za-z_][A-Za-z0-9_]*)*\z/;
+
+$SPEC{gen_class_source_code} = {
+    v => 1.1,
+    summary => 'Generate Perl source code to defined a class',
+    description => <<'_',
+
+_
+    args => {
+        name => {
+            schema  => ['str*', match=>$re_ident],
+            req => 1,
+        },
+        parent => {
+            schema  => ['str*', match=>$re_ident],
+        },
+        attributes => {
+            schema  => ['hash*', match=>$re_ident],
+            default => {},
+        },
+        variant => {
+            schema => ['str*', in=>[qw/classic Mo Moo Moose/]],
+            default => 'classic',
+        },
+    },
+    result_naked => 1,
+};
+sub gen_class_source_code {
+    my %args = @_;
+
+    # XXX schema
+    my $variant = $args{variant} // 'classic';
+
+    my @res;
+
+    push @res, "package $args{name};\n";
+    if ($variant =~ /^Mo/) {
+        push @res, "use $variant;\n";
+    }
+
+    if ($args{parent}) {
+        if ($variant =~ /^Mo/) {
+            push @res, "extends '$args{parent}';\n";
+        } else {
+            push @res, "use parent qw(", $args{parent}, ");\n";
+        }
+    }
+
+    if ($variant eq 'classic') {
+        push @res, q[sub new { my $class = shift; bless {@_}, $class; }], "\n";
+    }
+
+    my $attrs = $args{attributes} // {};
+    for my $name (sort keys %$attrs) {
+        if ($variant =~ /^Mo/) {
+            push @res, "has $name => (is=>'rw');\n";
+        } else {
+            push @res, "sub $name {}\n";
+        }
+    }
+
+    join("", @res);
+}
+
+1;
+# ABSTRACT:
+
+=head1 SYNOPSIS
+
+ use Class::GenSource qw(gen_class_source_code);
+
+ say gen_class_source_code(
+     name => 'My::Class',
+     attributes => {
+         foo => {},
+         bar => {},
+         baz => {},
+     },
+ );
+
+Will print something like:
+
+ package My::Class;
+
+ sub new { my $class = shift; bless {@_}, $class }
+ sub foo {}
+ sub bar {}
+ sub baz {}
+
+
+=head1 DESCRIPTION
